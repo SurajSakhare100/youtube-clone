c61ee6ab195ab393aaeb546601f883abf9db14be
suraj sakhare <sakharesuraj10@gmail.com>
2024-02-15 20:49:42 +0530

    intial commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-red-500">
+      YOUTUBE CLONE
+    </h1>
+    </div>
+  )
+}
+
+export default Home
