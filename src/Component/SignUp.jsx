ef763566652270ee775e3d00a7ab95befd4f74e1
suraj sakhare <sakharesuraj10@gmail.com>
2024-02-16 22:33:09 +0530

    Sign up page added

--- src/Component/SignUp.jsx
+++ src/Component/SignUp.jsx
@@ -3,10 +3,7 @@ import React from 'react'
 function SignUp() {
   return (
     <div className='h-screen flex flex-col w-100 items-center justify-center'>
-     <h1 className='text-center'>Sign Up</h1> 
-     <h1>hi  new sihn up page </h1>
-     <h1>hi  new sihn up page </h1>
-     <h1>hi  new sihn up page </h1>
+     <h1 className='text-center'>Sign Up</h1>
      <h1></h1>
     </div>
   )
