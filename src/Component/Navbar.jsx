7905716bb073510a4777549f64d6d0be13561210
suraj sakhare <sakharesuraj10@gmail.com>
2024-03-02 22:13:21 +0530

    nav added

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,42 @@
+import { faBars, faMicrophone, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+import youtube from '../assets/youtube.png'
+import { faSpeakap, faSpeakerDeck } from '@fortawesome/free-brands-svg-icons'
+
+function Navbar() {
+    return (
+        <>
+            <div className='flex flex-row items-center justify-between bg-black py-2 px-4'>
+                <div className='flex flex-row gap-2 items-center'>
+                    <FontAwesomeIcon icon={faBars} className='hover:rounded-full hover:bg-slate-900 text-md p-3 text-white' />
+                    <img src={youtube} width={30} height={20} />
+                    <p className='text-xl font-bold font-sans text-white'>YOUTUBE <sup className='text-sm font-normal'>IN</sup></p>
+                </div>
+                <div className='md:flex md:gap-4 md:items-center hidden'>
+                    <div className='h-full flex items-center justify-center relative '>
+                        <input
+                            type="text"
+                            placeholder='Search'
+                            className='md:w-[350px] lg:w-[500px] xl:w-[700px] md:h-full bg-black rounded-3xl border border-slate-100 py-2 px-2 outline-none' />
+                        <div className='h-full bg-[#222222] absolute right-0 top-0 rounded-3xl border border-slate-100 px-6 flex items-center justify-center rounded-l-none '>
+                            <FontAwesomeIcon icon={faSearch} className='text-white md:w-[20px] md:h-[20px]' />
+                        </div>
+                    </div>
+                    <div className='ml-2'>
+                        <FontAwesomeIcon icon={faMicrophone} className='text-white w-[30px] h-[20px] bg-[#262626] px-1 py-2 rounded-full' />
+                    </div>
+                </div>
+                <div>
+                    <div className='flex flex-row items-center justify-center gap-6'>
+                        <img src="src/assets/upload.png" className='w-[25px]' />
+                        <img src="src/assets/notification.png" className='w-[25px]' />
+                        <img src="src/assets/jack.png" className='w-[30px] h-[30px] rounded-full' />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,108 @@
 import React from 'react'
+import Navbar from '../../Component/Navbar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
 function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-red-500">
-      YOUTUBE CLONE 007
-    </h1>
-    </div>
+    <>
+      <Navbar />
+      <div className='w-full h-full flex flex-row'>
+        <div className='sm:w-1/6 bg-black sm:h-full w-[40px] h-full'>
+          <ul className='w-full px-2 border-b border-white pb-2'>
+            <li className='flex gap-4 items-center px-2 py-2 hover:bg-slate-600 rounded-lg'>
+              <FontAwesomeIcon icon={faHome} className='text-white'/>
+              <p className='md:text-white md:text-xl'>Home</p>
+            </li>
+            <li className='flex gap-4 items-center px-2 py-2 hover:bg-slate-600 rounded-lg'>
+              <FontAwesomeIcon icon={faHome} className='text-white'/>
+              <p className='md:text-white md:text-xl'>Home</p>
+            </li>
+            <li className='flex gap-4 items-center px-2 py-2 hover:bg-slate-600 rounded-lg'>
+              <FontAwesomeIcon icon={faHome} className='text-white'/>
+              <p className='md:text-white md:text-xl'>Home</p>
+            </li>
+          </ul>
+          <ul className='w-full px-2 border-b border-white pb-2'>
+            <li className='flex gap-4 items-center px-2 py-2 hover:bg-slate-600 rounded-lg'>
+              <FontAwesomeIcon icon={faHome} className='text-white'/>
+              <p className='md:text-white md:text-xl'>Home</p>
+            </li>
+            <li className='flex gap-4 items-center px-2 py-2 hover:bg-slate-600 rounded-lg'>
+              <FontAwesomeIcon icon={faHome} className='text-white'/>
+              <p className='md:text-white md:text-xl'>Home</p>
+            </li>
+            <li className='flex gap-4 items-center px-2 py-2 hover:bg-slate-600 rounded-lg'>
+              <FontAwesomeIcon icon={faHome} className='text-white'/>
+              <p className='md:text-white md:text-xl'>Home</p>
+            </li>
+          </ul>
+          <ul className='w-full px-2 border-b border-white pb-2'>
+            <li className='flex gap-4 items-center px-2 py-2 hover:bg-slate-600 rounded-lg'>
+              <FontAwesomeIcon icon={faHome} className='text-white'/>
+              <p className='md:text-white md:text-xl'>Home</p>
+            </li>
+            <li className='flex gap-4 items-center px-2 py-2 hover:bg-slate-600 rounded-lg'>
+              <FontAwesomeIcon icon={faHome} className='text-white'/>
+              <p className='md:text-white md:text-xl'>Home</p>
+            </li>
+            <li className='flex gap-4 items-center px-2 py-2 hover:bg-slate-600 rounded-lg'>
+              <FontAwesomeIcon icon={faHome} className='text-white'/>
+              <p className='md:text-white md:text-xl'>Home</p>
+            </li>
+            <li className='flex gap-4 items-center px-2 py-2 hover:bg-slate-600 rounded-lg'>
+              <FontAwesomeIcon icon={faHome} className='text-white'/>
+              <p className='md:text-white md:text-xl'>Home</p>
+            </li>
+            <li className='flex gap-4 items-center px-2 py-2 hover:bg-slate-600 rounded-lg'>
+              <FontAwesomeIcon icon={faHome} className='text-white'/>
+              <p className='md:text-white md:text-xl'>Home</p>
+            </li>
+          </ul>
+          <ul className='w-full px-2 border-b border-white pb-2'>
+            <li>Subscription</li>
+            <li className='flex gap-4 items-center px-2 py-2 hover:bg-slate-600 rounded-lg'>
+              <FontAwesomeIcon icon={faHome} className='text-white'/>
+              <p className='md:text-white md:text-xl'>Home</p>
+            </li>
+            <li className='flex gap-4 items-center px-2 py-2 hover:bg-slate-600 rounded-lg'>
+              <FontAwesomeIcon icon={faHome} className='text-white'/>
+              <p className='md:text-white md:text-xl'>Home</p>
+            </li>
+            <li className='flex gap-4 items-center px-2 py-2 hover:bg-slate-600 rounded-lg'>
+              <FontAwesomeIcon icon={faHome} className='text-white'/>
+              <p className='md:text-white md:text-xl'>Home</p>
+            </li>
+            <li className='flex gap-4 items-center px-2 py-2 hover:bg-slate-600 rounded-lg'>
+              <FontAwesomeIcon icon={faHome} className='text-white'/>
+              <p className='md:text-white md:text-xl'>Home</p>
+            </li>
+            <li className='flex gap-4 items-center px-2 py-2 hover:bg-slate-600 rounded-lg'>
+              <FontAwesomeIcon icon={faHome} className='text-white'/>
+              <p className='md:text-white md:text-xl'>Home</p>
+            </li>
+          </ul>
+        </div>
+        <div className='sm:w-3/4 sm:px-4 w-3/4 mx-auto mt-4 sm:grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4
+      gap-2'>
+          <div className='w-full'>
+            <img src="src/assets/thumbnail1.png" className='sm:w-full sm:px-2' />
+          </div>
+          <div className='w-full'>
+            <img src="src/assets/thumbnail1.png" className='sm:w-full sm:px-2' />
+          </div>
+          <div className='w-full'>
+            <img src="src/assets/thumbnail1.png" className='sm:w-full sm:px-2' />
+          </div>
+          <div className='w-full'>
+            <img src="src/assets/thumbnail1.png" className='sm:w-full sm:px-2' />
+          </div>
+          <div className='w-full'>
+            <img src="src/assets/thumbnail1.png" className='sm:w-full sm:px-2' />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
