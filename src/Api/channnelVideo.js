fa7989d8a32c0d278f0543d95c658716247d8905
suraj sakhare <sakharesuraj10@gmail.com>
2024-08-07 19:37:22 +0530

    api key updated

--- src/Api/channnelVideo.js
+++ src/Api/channnelVideo.js
@@ -1,16 +1,20 @@
+// Import the API key from the environment configuration
 import { API_KEY } from "..";
 
 const fetchVideos = async (channelId) => {
     try {
+        // Log the API key to verify it is correctly imported
+        console.log('YouTube API Key:', API_KEY);
+        
         const MAX_RESULTS = 50; 
         const video_url =
             `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=snippet&channelId=${channelId}&maxResults=${MAX_RESULTS}`;
         const response = await fetch(video_url);
         const data = await response.json();
         return data.items;
-    }
-    catch (error) {
+    } catch (error) {
         console.log('Error fetching videos:', error);
     }
 };
+
 export default fetchVideos;
