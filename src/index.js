86fa3b9c21b254ea7f225b8c7bd8a5925061ddee
suraj sakhare <sakharesuraj10@gmail.com>
2024-08-07 19:41:35 +0530

    api key updated

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-export const API_KEY = import.meta.env.VITE_YOUTUBE_API;
+export const API_KEY = import.meta.env.YOUTUBE_API;
 console.log(API_KEY); // This will log the actual API key value in production
